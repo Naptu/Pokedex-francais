76e74e8e841a5de5e659f38327639b84e991c6a5
Naptu <nadroj14@gmail.com>
2014-05-18 12:21:20 +0200

    Charmender's family
    .

---
+++ charmendersfamily.js
@@ -0,0 +1,17 @@
+a= { 
+    'salamèche': {
+        nom: 'Salamèche',
+        types: ['Feu'],
+        talents: ['Brasier','\'\'Force Soleil\'\''] 
+    },
+    'reptincel': {
+        nom: 'Reptincel',
+        types: ['Feu'],
+        talents: ['Brasier','\'\'Force Soleil\'\''] 
+    },
+    'dracaufeu': {
+        nom: 'Dracaufeu',
+        types: ['Feu','Vol'],
+        talents: ['Brasier','\'\'Force Soleil\'\''] 
+    }
+}
